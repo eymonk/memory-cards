a68d7da698bbaba1122ecfcc26ec19d63b7f66c2
JaySun0 <jaysun0@yandex.ru>
2023-03-11 08:39:40 +0300

    feat: add level messages translation

--- components/message/message.js
+++ components/message/message.js
@@ -3,6 +3,7 @@ import { openGallery } from '../gallery/gallery.js';
 import { closeMenu } from '../menu/menu.js';
 import { changeLevel } from '../cards/cards.js';
 import changeLanguage from '../languages/languages.js';
+import { levelMessagesRU, levelMessagesEN } from './level-message.js';
 
 const messageContainer = document.querySelector('.message__container');
 const messageControlsStart = document.querySelector('.message__controls_start');
@@ -50,33 +51,9 @@ function closeMessage() {
 
 function showLevelMessage() {
     let text, title;
-    switch(state.level) {
-        case 1:
-            title = 'Good👍';
-            text = 'Now choose what to do next - show photos from the cards or go to the next level right away.<br><br>(The next level contains all the photos from this level.)';
-            break;
-        case 2:
-            title = 'Well done!';
-            text = 'It was a bit harder, but still pretty easy!😉 <br><br> Now take some rest watching these exciting photos and then go to the next level!';
-            break;
-        case 3:
-            title = 'Excellent!';
-            text = 'Now I see that you have a pretty sharp mind! <br><br> 😎C O O L😎';
-            break;
-        case 4:
-            title = 'WOW!!!'
-            text = 'Only 1 level left to win the whole game! <br><br> 😮😮😮';
-            break;
-        case 5:
-            title = 'UNBELIEVABLE ❗❕❗'
-            text = `You proved to be a real MIND! <br> I'm truly astonished! 😲 <br><br> Thanks for your time. I hope you liked the game! Well, at least it trained your cognitive skills a little bit😊 <br><br> Good luck!`;
-            break;
-        default:
-            title = 'Great!';
-            text = 'You seem to be pretty clever! I like you😊';
-    }
-
-    showMessage(text, title, 'level-next');
+    const messages = state.language === 'en' ? levelMessagesEN : levelMessagesRU;
+    const message = messages[state.level];
+    showMessage(message.text, message.title, 'level-next');
 }
 
 
