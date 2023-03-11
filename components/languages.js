8d3d8924e0d4d47a65dd7145f7b10865521d3530
JaySun0 <jaysun0@yandex.ru>
2023-03-11 07:47:57 +0300

    feat: add translation functional

--- components/languages.js
+++ components/languages.js
@@ -1,7 +1,23 @@
 import state from './state.js';
 
+const nodesToTranslate = [];
+
+function addNodeToTranslate(nodeClassName, en, ru) {
+    const newNode = {
+        nodeClassName,
+        en,
+        ru
+    }
+    nodesToTranslate.push(newNode);
+}
+
 function changeLanguage() {
     state.language = state.language === 'en' ? 'ru' : 'en';
+    nodesToTranslate.forEach(node => {
+        const element = document.querySelector(`.${node.nodeClassName}`);
+        element.textContent = node[state.language];
+    });
 }
+
 
 export default changeLanguage;
